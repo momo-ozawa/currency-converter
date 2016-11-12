d788181207e0fc4510bb2cfbcafa2ef240c7c707
Momo Ozawa <mozawa92@gmail.com>
2016-11-12 11:23:06 +0900

    Set up model for exchange rates

---
+++ app/src/main/java/com/mozawa/coineyapp/data/model/Exchange.java
@@ -0,0 +1,15 @@
+package com.mozawa.coineyapp.data.model;
+
+public class Exchange {
+
+    double usd;
+    double eur;
+    double jpy;
+    double aud;
+    double inr;
+    double cad;
+    double zar;
+    double nzd;
+    double cny;
+
+}
---
+++ app/src/main/java/com/mozawa/coineyapp/data/model/ExchangeResponse.java
@@ -0,0 +1,16 @@
+package com.mozawa.coineyapp.data.model;
+
+public class ExchangeResponse {
+
+    Exchange gbp;
+    Exchange usd;
+    Exchange eur;
+    Exchange jpy;
+    Exchange aud;
+    Exchange inr;
+    Exchange cad;
+    Exchange zar;
+    Exchange nzd;
+    Exchange cny;
+
+}
