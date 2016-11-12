e8ec7595cba38fb12917d0aa76ce3dcd2425029a
Momo Ozawa <mozawa92@gmail.com>
2016-11-12 17:50:59 +0900

    Create a util class to unsubscribe from Rx subscription

---
+++ app/src/main/java/com/mozawa/coineyapp/util/RxUtil.java
@@ -0,0 +1,13 @@
+package com.mozawa.coineyapp.util;
+
+import rx.Subscription;
+
+public class RxUtil {
+
+    public static void unsubscribe(Subscription subscription) {
+        if (subscription != null && !subscription.isUnsubscribed()) {
+            subscription.unsubscribe();
+        }
+    }
+
+}
