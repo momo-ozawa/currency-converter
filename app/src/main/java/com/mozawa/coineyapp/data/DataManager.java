faa27ad5d29e2d16bb1ca75360ce70ce7005cbf3
Momo Ozawa <mozawa92@gmail.com>
2016-11-14 00:09:53 +0900

    Add a comment

--- app/src/main/java/com/mozawa/coineyapp/data/DataManager.java
+++ app/src/main/java/com/mozawa/coineyapp/data/DataManager.java
@@ -18,6 +18,10 @@ public class DataManager {
     }
 
     public Observable<HashMap<String, HashMap<String, Double>>> getMap() {
+        // I couldn't really figure out how to map dynamic keys. The nested HashMap is so unwieldy.
+        // Maybe I'll take up J.Wharton's suggestion and implement a Gson type adapter
+        // some time later...
+        // https://www.reddit.com/r/androiddev/comments/2znwcs/question_parsing_json_with_dynamic_key/cpkpnhl/
         return coineyService.getExchangeRates();
     }
 }
